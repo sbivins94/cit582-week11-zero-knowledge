34dad63092b7a0852eeaedd3080050d096b0374c
sbivins94 <bivins.sean7@gmail.com>
2022-04-02 13:45:37 -0600

    init commit

---
+++ ZK_equality.py
@@ -0,0 +1,12 @@
+from zksk import Secret, DLRep
+from zksk import utils
+
+def ZK_equality(G,H):
+
+    #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
+
+    #Generate a NIZK proving equality of the plaintexts
+
+    #Return two ciphertexts and the proof
+    return (C1,C2), (D1,D2), zk_proof
+
