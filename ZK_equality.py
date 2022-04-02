4c8785806b2005b6aae460712829be30a903d2ba
sbivins94 <bivins.sean7@gmail.com>
2022-04-02 14:48:12 -0600

    just removed .value from Secret objects

--- ZK_equality.py
+++ ZK_equality.py
@@ -7,11 +7,11 @@ def ZK_equality(G,H):
     m = Secret(utils.get_random_num(bits=128))
 
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
-    C1 = r1.value * G
-    C2 = r1.value * H + m.value * G
+    C1 = r1 * G
+    C2 = r1 * H + m * G
 
-    D1 = r2.value * G
-    D2 = r2.value * H + m.value * G
+    D1 = r2 * G
+    D2 = r2 * H + m * G
 
     #Generate a NIZK proving equality of the plaintexts
     zk_proof = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
